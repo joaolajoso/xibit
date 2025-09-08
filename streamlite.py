f435703dd04e22ca05a0e2b983df335bc73593d2
João Lajoso <54990286+joaolajoso@users.noreply.github.com>
2025-09-08 05:54:29 +0100

    Update streamlite.py

--- streamlite.py
+++ streamlite.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from supabase import create_client
 
 # Inicializa conexão Supabase e mantém com cache
@@ -17,18 +18,15 @@ st.title("Upload e Salvar CSV no Supabase")
 uploaded_file = st.file_uploader("Escolha um arquivo CSV", type="csv")
 
 if uploaded_file is not None:
-    # Lê CSV para DataFrame pandas
     df = pd.read_csv(uploaded_file)
     st.write("Visualização dos dados carregados:")
     st.dataframe(df)
 
     if st.button("Salvar no Supabase (tabela raw)"):
-        # Insere dados na tabela 'raw' do Supabase
-        # Convertendo DataFrame para lista de dicionários
-        data = df.to_dict(orient="records")
+        data = df.where(pd.notnull(df), None).to_dict(orient="records")
         response = supabase.table("raw").insert(data).execute()
-
         if response.status_code == 201:
             st.success("Dados salvos com sucesso na tabela raw do Supabase!")
         else:
             st.error(f"Erro ao salvar dados: {response.status_code}")
+
